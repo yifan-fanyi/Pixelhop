7ea20c1efb69ac344da77c971a1082b8ea419573
yifan-fanyi <yifanwang0916@outlook.com>
2019-11-11 20:55:55 -0800

    Update ada_kmeans.py

--- src/framework/ada_kmeans.py
+++ src/framework/ada_kmeans.py
@@ -13,6 +13,8 @@ from sklearn.cluster import MiniBatchKMeans, KMeans
 from sklearn.metrics import log_loss as LL
 from collections import Counter
 from sklearn.linear_model import LogisticRegression
+import warnings
+warnings.filterwarnings('ignore')
 
 def encode_soft(label, total=None):
     soft_labels = np.zeros(len(total))
@@ -56,12 +58,19 @@ def Comupte_Cross_Entropy(X, Y, total=None):
     return LL(true_indicator,pred_soft)/math.log(cls_num)
 
 def Comupte_Cross_Entropy1(X, Y):
+    cls_num = np.unique(Y).shape[0]
+    samp_num = Y.size
     if np.unique(Y).shape[0] == 1: #alread pure
         return 0
-    clf = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial').fit(X, Y)
+    clf = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial',class_weight='balanced').fit(X, Y.reshape(-1,))
     prob = clf.predict_proba(X)
-    logprob = np.log10(prob[Y])/np.log10(2)
-    return -np.mean(logprob)
+    #print(prob[0:5])
+    #logprob = np.log10(prob[Y])/np.log10(2)
+    #print(logprob[0:5])
+    true_indicator = np.zeros((samp_num,cls_num))
+    true_indicator[np.arange(samp_num),Y[:,0]] = 1
+    #print(LL(true_indicator,prob)/math.log(cls_num))
+    return LL(true_indicator,prob)/math.log(cls_num)
 
 #only for 2
 def Compute_Weight(Y):
@@ -74,8 +83,8 @@ def Multi_Trial(X, sep_num=2, batch_size=None, trial=6, total=None):
     init = ['k-means++','random','k-means++','random','k-means++','random']
     H = X['H']
     center = []
+    flag = 0
     for i in range(trial):
-        tH = 0.0
         if batch_size == None:
             kmeans = KMeans(n_clusters=sep_num,n_jobs=10,init = init[i]).fit(X['Data'])
         else:
@@ -85,16 +94,18 @@ def Multi_Trial(X, sep_num=2, batch_size=None, trial=6, total=None):
         counting = np.array(Counter(k_labels.tolist()).most_common(np.unique(k_labels).size))[:,1]
         if np.min(counting)>int(0.05*X['Data'].shape[0]):
             weight = Compute_Weight(kmeans.labels_)
+            tH = 0.0
             for k in range(sep_num):
-                tH = tH + weight[k]*Comupte_Cross_Entropy1(X['Data'][kmeans.labels_ == k], X['Label'][kmeans.labels_ == k])
+                tH += weight[k]*Comupte_Cross_Entropy1(X['Data'][kmeans.labels_ == k], X['Label'][kmeans.labels_ == k])
             #tH /= float(sep_num)
             if tH < H:
-                H = np.copy(tH)
-                center = kmeans.cluster_centers_
-                label = kmeans.labels_
+                H = tH
+                center = kmeans.cluster_centers_.copy()
+                label = kmeans.labels_.copy()
+                flag = 1
                 print("            <Multi_Trial {}>    Found a separation better than original!".format(i))
                 print("            <Multi_Trial {}>    New ce = {}".format(i,H))
-    if len(center)==0:
+    if flag == 0:
         return []
     subX = []
     for i in range(sep_num):
@@ -118,15 +129,14 @@ def Ada_KMeans(X, Y, trial=6, batch_size=10000, minS=0.1, maxN=50, limit=0.5, ma
     
     print("        > Start!")
     while N < maxN and myiter < maxiter:
-        #print("        > Ite--{}: {}".format(myiter,H))
+        print("        > Ite--{}".format(myiter))
         idx = np.argmax(np.array(H))
         if Continue_split(H[idx], limit) == False: # continue to split?
             print("        > Finish splitting!")
             break
-        print(idx, Hidx, H)
         if data[Hidx[idx]]['Data'].shape[0] < int(minS*Num_sample): # if this cluster has too few sample, change the next largest
             print("        > Ite--{}: Too small! continue for the next largest".format(myiter))
-            H[idx] = -2
+            H[idx] = -H[idx]
             continue
         subX = Multi_Trial(data[Hidx[idx]], batch_size=batch_size, trial=trial)
         if len(subX)!=0:
@@ -143,34 +153,34 @@ def Ada_KMeans(X, Y, trial=6, batch_size=10000, minS=0.1, maxN=50, limit=0.5, ma
             myiter += 1
         else:
             print("        > Ite--{}: Don't split! continue for the next largest".format(myiter))
-            H[idx] = -1
+            H[idx] = -H[idx]
+    return Merge(Y, data, Hidx)
 
-    return Merge(data, Hidx)
-
-def Merge(data, Hidx):
+def Merge(Y, data, Hidx):
     centroid = []
-    X = []
     Y = []
     cluster_label = []
-    label = []
+    n0 = 9
+    n1 = 1
     for i in range(0, len(Hidx)-1):
-        if data[Hidx[i]]['H'] == -2:
-            continue
-        X.append(data[Hidx[i]]['Data'])
         Y.append(data[Hidx[i]]['Label'])
-        centroid.append(data[Hidx[i]]['Centroid'])
+        centroid.append(data[Hidx[i]]['Centroid'].reshape(-1))
         t = data[Hidx[i]]['Label']
-        if t[t == 0].shape[0] > t[t == 1].shape[0]:
+        if t[t == 0].shape[0]/n0 > t[t == 1].shape[0]/n1:
             cluster_label.append(0)
-        elif t[t == 0].shape[0] < t[t == 1].shape[0]:
+        elif t[t == 0].shape[0]/n0 < t[t == 1].shape[0]/n1:
             cluster_label.append(1)
-        label.append(i*np.ones(data[Hidx[i]]['Label'].shape[0]))
-    X = np.array(X)
-    X = X.reshape(-1, X.shape[-1])
+        if i == 0:
+            X = data[Hidx[i]]['Data']
+            label = i*np.ones((data[Hidx[i]]['Label'].shape[0],1))
+        else:
+            X = np.concatenate((X,data[Hidx[i]]['Data']),axis=0)
+            label = np.concatenate((label, i*np.ones((data[Hidx[i]]['Label'].shape[0],1))), axis=0)    
     Y = np.array(Y)
+    Y = Y.reshape(-1,1)
     centroid = np.array(centroid)
     cluster_label = np.array(cluster_label)
-    label = np.array(label)
+    cluster_label = cluster_label.reshape(-1,1)
     return X, Y, label, centroid, cluster_label
 
 
